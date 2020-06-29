868617f51a701e214051b819ff0cc2ecf945a8e7
Scalvertuk <s.calvertuk@gmail.com>
2020-06-29 14:36:04 +0100

    Refined bounding box
    Refined bounding box to become a 50 mile circle incorporate the curvature of the earth when finding users

--- src/main/java/com/example/RecordScanner/RecordScannerApplication.java
+++ src/main/java/com/example/RecordScanner/RecordScannerApplication.java
@@ -10,18 +10,18 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class RecordScannerApplication {
-	public static void main(String[] args) throws IOException  {
+	public static void main(String[] args)/* throws IOException */ {
 		SpringApplication.run(RecordScannerApplication.class, args);
 
 			//FOR TESTING METHODS
-
+/*
 		String url = "https://bpdts-test-app.herokuapp.com/users";
 		String cityUrl = "https://bpdts-test-app.herokuapp.com/city/London/users";
 
 		Record.findUsers(url,cityUrl);
 			System.out.println("GET USERS DONE");
 			System.out.println("GET CITY DONE");
-
+*/
 
 		//follow this link to the http://localhost:9099/retrieve-users to view the output
 		//to view API method go to http://localhost:9099/swagger-ui.html to view Get method
